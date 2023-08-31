9bdc54d60f72e258d65f297cc0068cdd41bec463
unknown <putrigsi30a@gmail.com>
2023-08-31 11:15:02 +0700

    membuat halaman transaksi

---
+++ resources/views/transaksi.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ini halaman transaksi</title>
+</head>
+<body>
+    <h1>halaman transaksi</h1>
+</body>
+</html>
